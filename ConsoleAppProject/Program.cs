1700815524fdd03956ef349954f7bbd763092a7e
Barna Gergely <h144988@stud.u-szeged.hu>
2023-09-29 07:26:05 +0200

    sokminden

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,47 +1,101 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Globalization;
 using System.Text.RegularExpressions;
-using ConsoleAppProject.Team;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace ConsoleAppProject
 {
 
     internal class Program
     {
-        
+        static IEnumerable<TeamRecord>? dataBase;
         /*
             - Init
                  - parancssori paraméterek betöltése, hibás input kezelés (2 paraméter adható pontosan meg)
                  - adatok betöltése CSV fájlból, hibás input kezelés
             - TerminalLoop
          */
-         static void terminate(string message) {
-                    Console.WriteLine(message);
-                    System.Environment.Exit(1);
-         }
+        static void terminate(string message)
+        {
+            Console.WriteLine(message);
+            System.Environment.Exit(1);
+        }
 
-         static void init(){
-            
-         }
+        static void init()
+        {
 
-         static void validateParameters(string[] args)
-         {
-         }
+        }
 
-         static void terminalLoop(){
+        static void validateParameters(string[] args)
+        {
+        }
 
-         }
+        static void terminalLoop()
+        {
+
+        }
 
         static void team(string teamName)
         {
-            return;
+            IEnumerable<TeamRecord> teamQuery =
+                from record in dataBase
+                where record.Team == teamName
+                select record;
+
+            Console.WriteLine(teamQuery.ToList<TeamRecord>().Count());
         }
 
-        static void count(int , int) {
+        static void count(string columName, int lowerLimit, int upperLimit)
+        {
+            IEnumerable<TeamRecord> countQuery =
+                    from record in dataBase
+                    select record;
 
+            if (columName == "Age")
+            {
+                countQuery =
+                    from record in dataBase
+                    where record.Age >= lowerLimit && record.Age <= upperLimit
+                    select record;
+            }
+            else if (columName == "Height")
+            {
+                countQuery =
+                    from record in dataBase
+                    where record.Height >= lowerLimit && record.Age <= upperLimit
+                    select record;
+            }
+
+            else if (columName == "Weight")
+            {
+                countQuery =
+                    from record in dataBase
+                    where record.Weight >= lowerLimit && record.Age <= upperLimit
+                    select record;
+            }
+            else
+            {
+                Console.WriteLine("Column name not found");
+                return;
+            }
+
+            // eredmény kiíratás
+            foreach (var record in countQuery)
+            {
+                Console.WriteLine(record.ToString());
+            }
         }
 
-        static void average(string, string) {
-            
+
+
+
+
+
+        static void average(string a, string b)
+        {
+
         }
         static void Main(string[] args)
         {
@@ -50,43 +104,65 @@ namespace ConsoleAppProject
 
             #region init
             #region validateParameters
-                if(args == null || args.Length < 2) {
-                    terminate("Less than 2 parameters");
-                    return;
-                } else if(args.Length > 2 ) {
-                    terminate("More than 2 parameters");
-                    return;
-                } else {
-                    /* argumentumok tartalmának validálása
-                    // egyáltalán szükség van erre?
-                    foreach (string arg in args) {
-                        
-                    }
+            if (args == null || args.Length < 2)
+            {
+                terminate("Critical error: Less than 2 parameters");
+                return;
+            }
+            else if (args.Length > 2)
+            {
+                terminate("Critical error: More than 2 parameters");
+                return;
+            }
+            else
+            {
+                /* argumentumok tartalmának validálása
+                // egyáltalán szükség van erre?
+                foreach (string arg in args) {
 
-                     Regex locationRegex = new Regex(@"^(?:[a-zA-Z]\:|\\\\[\w\.]+\\[\w.$]+)\\(?:[\w]+\\)*\w([\w.])+$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-
-                    if (!locationRegex.Mach(args[0])) Console.WriteLine("this is not a valid file path");
-                    // TODO: ezt megcsinálni másra is
-                    */
-
-                    inputPath = args[0];
-                    outputPath = args[1];
                 }
+
+                 Regex locationRegex = new Regex(@"^(?:[a-zA-Z]\:|\\\\[\w\.]+\\[\w.$]+)\\(?:[\w]+\\)*\w([\w.])+$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+                if (!locationRegex.Mach(args[0])) Console.WriteLine("this is not a valid file path");
+                // TODO: ezt megcsinálni másra is
+                */
+
+                inputPath = args[0];
+                outputPath = args[1];
+            }
             #endregion
-            
+
 
             #region loading CSV file
-            // TODO hiba kezelés
-            try{
+            /* TODO hiba kezelés
+            try
+            {
 
-            } catch() {
+            }
+            catch ()
+            {
                 terminate("wrong file path or the file does not exists")
             }
-            using (var reader = new StreamReader(inputPath))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            */
+
+            try
             {
-                var records = csv.GetRecords<Team>();
+                using (var reader = new StreamReader(inputPath))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    csv.Context.RegisterClassMap<TeamMap>();
+                    var records = csv.GetRecords<TeamRecord>();
+                    dataBase = records.ToList<TeamRecord>();
+                }
             }
+            catch (System.IO.FileNotFoundException e)
+            {
+                terminate(e.Message);
+                return;
+            }
+
+            Console.WriteLine("csv successfully loaded.");
 
             #endregion
             #endregion
@@ -95,29 +171,40 @@ namespace ConsoleAppProject
 
             string userInput;
 
-                Regex teamRegex = new Regex(@"^team [a-zA-Z]+", RegexOptions.Compiled);
-                Regex countRegex = new Regex(@"^count [a-zA-Z]+ [1-9]+ [1-9]+", RegexOptions.Compiled);
-                Regex averageRegex = new Regex(@"^avarage [a-zA-Z]+ [1-9]+", RegexOptions.Compiled);
+            Regex teamRegex = new Regex(@"^team [a-zA-Z]+", RegexOptions.Compiled);
+            Regex countRegex = new Regex(@"^count [a-zA-Z]+ [0-9]+ [0-9]+$", RegexOptions.Compiled);
+            Regex averageRegex = new Regex(@"^avarage [a-zA-Z]+ [0-9]+$", RegexOptions.Compiled);
 
             do
             {
                 userInput = Console.ReadLine();
 
-                if(teamRegex.Matches(userInput).count() == 1){
+                if (teamRegex.Matches(userInput).Count() == 1)
+                {
                     string[] subStr = userInput.Split(' ');
 
                     team(subStr[1]);
 
-                } else if (countRegex.Matches(userInput).count() == 1) {
+                }
+                else if (countRegex.Matches(userInput).Count() == 1)
+                {
                     string[] subStr = userInput.Split(' ');
-                    count(subStr[1], subStr[2]);
+                    count(subStr[1], int.Parse(subStr[2]), int.Parse(subStr[3]));
 
-                } else if (averageRegex.Matches(userInput). count == 1) {
+                }
+                else if (averageRegex.Matches(userInput).Count == 1)
+                {
                     string[] subStr = userInput.Split(' ');
                     average(subStr[1], subStr[2]);
-                
-                } else {
-                    Console.WriteLine("the '"+userInput+"' command not found.");
+
+                }
+                else if (userInput == "stop")
+                {
+                    Console.WriteLine("Quitting");
+                }
+                else
+                {
+                    Console.WriteLine("The '" + userInput + "' command not found.");
                 }
 
 
--- ConsoleAppProject/Team.cs
+++ ConsoleAppProject/Team.cs
@@ -1,14 +1,80 @@
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using CsvHelper;
+using System.Globalization;
+
 namespace ConsoleAppProject
 {
-    // TODO: külön file-ba rakni
-    class Team {
-        int id;
-        string name;
-        string sex;
-        int age;
-        int height;
-        int weight;
-        string team;
-        string noc;
-    } 
+    public class IntConverter<T> : DefaultTypeConverter
+    {
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            if (text == "NA")
+            {
+                return 0;
+            }
+            else
+            {
+                return int.Parse(text);
+            }
+        }
+
+        public override string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+        {
+            return value.ToString();
+        }
+    }
+
+    public class FloatConverter<T> : DefaultTypeConverter
+    {
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            if (text == "NA")
+            {
+                return float.Parse("0", CultureInfo.InvariantCulture.NumberFormat);
+            }
+            else
+            {
+                return float.Parse(text, CultureInfo.InvariantCulture.NumberFormat);
+            }
+        }
+
+        public override string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+        {
+            return value.ToString();
+        }
+    }
+
+    public class TeamRecord
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public int Age { get; set; }
+        public float Height { get; set; }
+        public float Weight { get; set; }
+        public string Team { get; set; }
+        public string NOC { get; set; }
+
+        public virtual string? ToString()
+        {
+            return ID + " " + Name + " " + Sex + " " + Age + " " + Height + " " + Weight + " " + Team + " " + NOC;
+        }
+    }
+
+    public class TeamMap : ClassMap<TeamRecord>
+    {
+        public TeamMap()
+        {
+            Map(m => m.ID).TypeConverter<IntConverter<int>>().Name("ID");
+            Map(m => m.Name).Name("Name");
+            Map(m => m.Sex).Name("Sex");
+            Map(m => m.Age).TypeConverter<IntConverter<int>>().Name("Age");
+            Map(m => m.Height).TypeConverter<FloatConverter<float>>().Name("Height");
+            Map(m => m.Weight).TypeConverter<FloatConverter<float>>().Name("Weight");
+            Map(m => m.Team).Name("Team");
+            Map(m => m.NOC).Name("NOC");
+             
+        }
+    }
 }
